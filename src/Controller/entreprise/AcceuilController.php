24c5e53e5e156e2cdfafe6a10d60cec7ba923cba
MyriamFilia <filiamyriam@yahoo.fr>
2024-03-12 21:46:20 +0100

    Accueil Entreprise Front de le page d'accueil entreprise

--- src/Controller/entreprise/AcceuilController.php
+++ src/Controller/entreprise/AcceuilController.php
@@ -2,7 +2,19 @@
 
 namespace App\Controller\entreprise;
 
-class AcceuilController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AcceuilController extends AbstractController
 {
+
+    #[Route('/acceuil-entreprise', name: 'acceuil-entreprise')]
+    public function index(): Response
+    {
+        return $this->render(view: 'entreprise/acceuil.html.twig', parameters: [
+
+        ]);
+    }
 
 }
