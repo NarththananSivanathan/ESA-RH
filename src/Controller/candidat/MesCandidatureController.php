07ab965d8dd7998e13510dd5d1961bd845056400
Gheldie <wwwgheldie.mg@gmail.com>
2024-03-29 09:31:21 +0100

    mes candidature

--- src/Controller/candidat/MesCandidatureController.php
+++ src/Controller/candidat/MesCandidatureController.php
@@ -2,7 +2,37 @@
 
 namespace App\Controller\candidat;
 
-class MesCandidatureController
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CandidatureRepository;
+
+class MesCandidatureController extends AbstractController
 {
 
+    #[Route('/candidatures', name: 'app_candidatures')]
+    public function index(): Response
+    {
+        return $this->render('candidat\mesCandidatures.html.twig', [
+            
+        ]);
+    }
+
+
+
+
+    // public function acceuil(CandidatureRepository $candidatureRepository): Response
+    // {
+    //     // Récupérez le nombre de candidatures depuis votre base de données
+    //     $nombreCandidatures = $candidatureRepository->count([]);
+
+    //     return $this->render('candidat/acceuil.html.twig', [
+    //         'nombreCandidatures' => $nombreCandidatures,
+    //     ]);
+    // }
+
+
 }
+
+    
