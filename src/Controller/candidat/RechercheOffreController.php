2fa45e1d5dd5a19334622fc97dacb6310eb3656a
Gheldie <wwwgheldie.mg@gmail.com>
2024-03-29 09:24:24 +0100

    recherche offre

--- src/Controller/candidat/RechercheOffreController.php
+++ src/Controller/candidat/RechercheOffreController.php
@@ -2,7 +2,22 @@
 
 namespace App\Controller\candidat;
 
-class RechercheOffreController
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class RechercheOffreController extends AbstractController
 {
 
+    #[Route('/recherche', name: 'app_recherche')]
+    public function index(): Response
+    {
+        return $this->render('candidat\rechercheOffre.html.twig', [
+            
+        ]);
+    }
+    
 }
