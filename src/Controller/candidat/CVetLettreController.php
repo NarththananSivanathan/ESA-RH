7d30f518c950a21eda55abf56713568a4ab72675
Gheldie <wwwgheldie.mg@gmail.com>
2024-03-29 09:17:21 +0100

    confirmation cv

--- src/Controller/candidat/CVetLettreController.php
+++ src/Controller/candidat/CVetLettreController.php
@@ -2,7 +2,20 @@
 
 namespace App\Controller\candidat;
 
-class CVetLettreController
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+ 
+class CVetLettreController extends AbstractController
 {
 
+    #[Route('/cv', name: 'app_cv')]
+    public function index(): Response
+    {
+        return $this->render('candidat\CVetLettre.html.twig', [
+            
+        ]);
+    }
 }
