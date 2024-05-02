560d7d90d961c65301d99614fcb852dc36c9f4fb
NarththananSivanathan <narththanan.pro@gmail.com>
2024-05-02 13:47:18 +0200

    Crée offre Affichier les offre Quelle que path lien entre les page

--- src/Controller/Entreprise/AccueilController.php
+++ src/Controller/Entreprise/AccueilController.php
@@ -5,15 +5,27 @@ namespace App\Controller\Entreprise;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccueilController extends AbstractController
 {
 
     #[Route('/entreprise/accueil', name: 'accueil_entreprise')]
-    public function index(): Response
+    public function accueil(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render(view: 'entreprise/accueil.html.twig', parameters: [
 
+        $recruteur = $this->getUser();
+        $entreprise = $this->getUser()->getEntreprise();
+        $adresse = $this->getUser()->getAdresse();
+        $lastOffre = $this->getUser()->getEntreprise()->getOffres()->last();
+
+        $countOffre = $this->getUser()->getEntreprise()->getOffres()->count();
+        return $this->render(view: 'entreprise/accueil.html.twig', parameters: [
+            'recruteur' => $recruteur,
+            'entreprise' => $entreprise,
+            'adresse' => $adresse,
+            'lastOffre' => $lastOffre,
+            'countOffre' => $countOffre
         ]);
     }
 
--- src/Controller/Entreprise/CreeOffreController.php
+++ src/Controller/Entreprise/CreeOffreController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller\Entreprise;
 
+use App\Entity\Offre;
+use App\Form\OffreType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class CreeOffreController extends AbstractController
 {
-    #[Route('/cree-offre', name: 'cree-offre')]
-    public function index(): Response
+    #[Route('entreprise/cree-offre', name: 'cree-offre')]
+    public function creationOffre (Request $request, EntityManagerInterface $em, AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render(view: 'entreprise/creeOffre.html.twig', parameters: [
+        $offre = new Offre();
+        $form = $this->createForm(OffreType::class, $offre);
 
+        if ($request->isMethod('POST')) {
+            $allValues = $request->request->all();
+            $form->submit($allValues[$form->getName()]);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                /** @var Offre $offre */
+
+                $offre = $form->getData();
+
+                $offre->setDateCreation(new \DateTimeImmutable());
+                $offre->setIdEntreprise($this->getUser()->getEntreprise());
+
+                $em->persist($offre);
+                $em->flush();
+
+                $this->addFlash(
+                    'notice',
+                    'Votre offre a bien été créée.'
+                );
+
+                return $this->redirectToRoute('accueil_entreprise');
+            }
+        }
+        return $this->render(view: 'entreprise/creeOffre.html.twig', parameters: [
+            'form' => $form
         ]);
     }
 }
